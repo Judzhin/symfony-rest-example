821f7d5934f7e4143c5af2b41e47b95a1d1df148
Judzhin Miles <info@msbios.com>
2021-06-04 14:24:04 +0300

    Unit Test

--- tests/Unit/Entity/CustomerTest.php
+++ tests/Unit/Entity/CustomerTest.php
@@ -25,10 +25,18 @@ class CustomerTest extends TestCase
             ->withId($id = 1)
             ->withFirstName($firstName = $faker->firstName())
             ->withLastName($lastName = $faker->lastName())
+            ->withEmail($email = $faker->email())
+            ->withPhoneNumber($phoneNumber = $faker->phoneNumber())
+            ->withCreatedAt($createdAt = new \DateTime)
             ->build();
 
         $this->assertInstanceOf(Customer::class, $customer);
         $this->assertEquals($customer->getId(), $id);
         $this->assertEquals($customer->getFirstName(), $firstName);
+        $this->assertEquals($customer->getLastName(), $lastName);
+        $this->assertEquals($customer->getEmail(), $email);
+        $this->assertEquals($customer->getPhoneNumber(), $phoneNumber);
+        $this->assertInstanceOf(\DateTime::class, $customer->getCreatedAt());
+        $this->assertEquals($customer->getCreatedAt(), $createdAt);
     }
 }
